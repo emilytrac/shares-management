6dd2b8ccfd4d7b8981019568ad8e6ddbf1d1f0ae
Emily Tracey <emilytracey21@gmail.com>
2022-12-06 15:24:40 +0000

    A few updates

--- shares.sql
+++ shares.sql
@@ -15,4 +15,22 @@ shareType varchar (50),
 quantity int
 );
 
+insert into shares (shareId, shareName, marketPrice)
+values (101, 'Apple', 143.73),
+;
+
+insert into shares (shareId, shareName, marketPrice)
+values(102, 'Tesla', 178.55),
+(103, 'Microsoft', 249.57)
+;
+
+insert into customers (detailId, customerId, shareId, shareType, quantity)
+values (1, 201, 101, 'long-term', 100);
+
+insert into customers (detailId, customerId, shareId, shareType, quantity)
+values (2, 201, 102, 'long-term', 50),
+(3, 201, 103, 'long-term', 75);
+
+select * from shares;
+
 
